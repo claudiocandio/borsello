92a220924def9343cf826ae4d645db69a546341e
clc <clc@claudiocandio.it>
2019-09-07 11:00:39 +0200

    add docker iroha grpc postgres

---
+++ docker/iroha/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#while ! curl http://wallet-js-iroha-postgres:5432/ 2>&1 | grep '52'
+#do
+#done
+sleep 10
+# --overwrite-ledger
+#irohad --genesis_block genesis-mini.block.json --config config.docker --keypair_name $KEY --overwrite-ledger
+irohad --genesis_block genesis-mini.block.json --config config.docker --keypair_name $KEY
